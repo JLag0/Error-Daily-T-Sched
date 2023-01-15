082349481a0d4e249be3d54653fd441ab8a68fb4
JLag0 <jhonulysis48@gmail.com>
2023-01-16 00:28:59 +0800

    added roles

--- app/src/main/java/com/ulan/timetable/activities/TeamError.java
+++ app/src/main/java/com/ulan/timetable/activities/TeamError.java
@@ -1,6 +1,13 @@
 package com.ulan.timetable.activities;
 
+import static java.security.AccessController.getContext;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -9,10 +16,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.ulan.timetable.R;
 
-
 public class TeamError extends AppCompatActivity {
     private String teamName;
-    private String[] teamMembers;
+    private TeamMember[] teamMembers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,26 +26,63 @@ public class TeamError extends AppCompatActivity {
         setContentView(R.layout.activity_team);
 
         teamName = "App Development Team";
-        teamMembers = new String[] {"GONZALVO, KENNETH JASPER Q." ,"BARILLA, LAWRENCE JUSTINE C.", "BAUTISTA KAZUYUKI", "BROSAS, ALEXANDRA NICHOLE M." , "DELA CERNA, JENZY D.", "BELEN, RASSEL M.","LAGO, JHON ULYSIS M."};
+        teamMembers = new TeamMember[] {
+                new TeamMember("Gonzalvo, Kenneth Jasper Q.", new String[] {"Team Leader"}),
+                new TeamMember("Dela Cerna, Jenzy D.",new String[] {"System Analyst"}),
+                new TeamMember("Lago, Jhon Ulysis M.",new String[] {"Programmer"}),
+                new TeamMember("Bautista Kazuyuki",new String[] {"Graphic Designer"}),
+                new TeamMember("Brosas, Alexandra Nichole M.",new String[] {"Document Controller"}),
+                new TeamMember("Barilla, Lawrence Justine C.",new String[]{"Document Controller"})
+        };
 
         TextView teamNameView = findViewById(R.id.team_name);
         teamNameView.setText(teamName);
 
         ListView teamMembersView = findViewById(R.id.team_member);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, teamMembers);
+        TeamMemberAdapter adapter = new TeamMemberAdapter(this, teamMembers);
         teamMembersView.setAdapter(adapter);
     }
 }
 
+class TeamMember {
+    private String name;
+    private String[] roles;
 
+    public TeamMember(String name, String[] roles) {
+        this.name = name;
+        this.roles = roles;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String[] getRoles() {
+        return roles;
+    }
+}
 
+class TeamMemberAdapter extends ArrayAdapter<TeamMember> {
+    public TeamMemberAdapter(Context context, TeamMember[] teamMembers) {
+        super(context, 0, teamMembers);
+    }
 
-
-
-
-
-
-
-
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get the data item for this position
+        TeamMember teamMember = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_team, parent, false);
+        }
+        // Lookup view for data population
+        TextView name = convertView.findViewById(R.id.team_name);
+        TextView role = convertView.findViewById(R.id.role);
+        // Populate the data into the template view using the data object
+        name.setText(teamMember.getName());
+        String roles = TextUtils.join(", ", teamMember.getRoles());
+        role.setText(roles);
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
