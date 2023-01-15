222c23ba4152f97d47d476b1bd09cdb0779facf1
JLag0 <jhonulysis48@gmail.com>
2023-01-16 00:33:09 +0800

    added roles

--- app/src/main/java/com/ulan/timetable/activities/TeamError.java
+++ app/src/main/java/com/ulan/timetable/activities/TeamError.java
@@ -31,8 +31,8 @@ public class TeamError extends AppCompatActivity {
                 new TeamMember("Dela Cerna, Jenzy D.",new String[] {"System Analyst"}),
                 new TeamMember("Lago, Jhon Ulysis M.",new String[] {"Programmer"}),
                 new TeamMember("Bautista Kazuyuki",new String[] {"Graphic Designer"}),
-                new TeamMember("Brosas, Alexandra Nichole M.",new String[] {"Document Controller"}),
-                new TeamMember("Barilla, Lawrence Justine C.",new String[]{"Document Controller"})
+                new TeamMember("Brosas, Alexandra Nichole M. \n Barilla, Lawrence Justine C.",new String[] {"Document Controller"})
+
         };
 
         TextView teamNameView = findViewById(R.id.team_name);
